7722fd081eb89ec9b9c76f62f04527b4d8b4d825
LeonidSimonov <ls@advanced-store.de>
2014-05-28 14:05:29 +0200

    changed facade

---
+++ src/AdvancedStore/Facades/ApiClientFacade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AS-LS
+ * Date: 28.05.14
+ * Time: 12:19
+ */
+
+namespace AdvancedStore\ApiClient\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+
+class ApiClientFacade extends Facade{
+    protected static function getFacadeAccessor() { return 'apiClient'; }
+}
---
+++ src/controllers/ApiClientController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AS-LS
+ * Date: 28.05.14
+ * Time: 12:26
+ */
+
+namespace AdvancedStore\ApiClient\Controllers;
+
+class ApiClientController{
+
+
+    public function getUserPermissions(){
+        return "test";
+    }
+
+}
