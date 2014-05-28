dae83413ccc1d5367d224e6e4a725d67e3404568
LeonidSimonov <ls@advanced-store.de>
2014-05-28 12:11:20 +0200

    api-client start

---
+++ src/AdvancedStore/ApiClient/ApiClientServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace AdvancedStore\ApiClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiClientServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('advanced-store/api-client');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
