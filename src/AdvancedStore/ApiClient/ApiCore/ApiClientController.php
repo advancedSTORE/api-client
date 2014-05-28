e81fe5f0615639086c1d40ada1f357e85a62c1c9
LeonidSimonov <ls@advanced-store.de>
2014-05-28 18:07:05 +0200

    added config file, changes controller logic to fetch from oauth2Client

--- src/AdvancedStore/ApiClient/ApiCore/ApiClientController.php
+++ src/AdvancedStore/ApiClient/ApiCore/ApiClientController.php
@@ -10,9 +10,48 @@ namespace AdvancedStore\ApiClient\ApiCore;
 
 class ApiClientController{
 
+    private $apiResponse = null;
+
+    private $userPermissions = null;
+
+    public function setApiResponse( $apiResponse ){
+        $this->apiResponse = $apiResponse;
+    }
 
     public function getUserPermissions(){
-        return "test";
+//        $this->userPermissions = ['ad4mat.admin.dashboard','ad4mat.admin.users.read'];
+
+        $this->userPermissions = $this->extractUserPermissions();
+
+        return $this->userPermissions;
+    }
+
+    /**
+     * @return array
+     * Extract and merge all user permissions from user roles(groups) and partner roles
+     * into a single array with unique values.
+     */
+    private function extractUserPermissions( ){
+        $groupPermissions = $this->getAppPermissions( $this->apiResponse->groups );
+        $partnerPermissions = $this->getAppPermissions( $this->apiResponse->partnerRoles );
+
+        $userPermissions = array_merge( $groupPermissions, $partnerPermissions );
+
+        return array_unique( $userPermissions );
+    }
+
+
+    private function getAppPermissions( $userRoles ){
+
+        $permissionArray = [];
+
+        foreach( $userRoles as $role ){
+            foreach( $role->appPermissions->toArray() as $appPermission ){
+                $permissionArray[] = $appPermission['appPermissionName'];
+            }
+        }
+
+        return $permissionArray;
     }
 
 }
---
+++ src/config/apiClientConfig.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AS-LS
+ * Date: 28.05.14
+ * Time: 18:02
+ */
+
+
+return [
+    'OAuth2Client'      =>  null,
+];
